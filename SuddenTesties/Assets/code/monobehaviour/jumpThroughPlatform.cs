ba08f115a41257ebd6367318b107195fd02c47f1
Nerfertius <nerfertius@gmail.com>
2017-07-08 01:33:52 +0200

    NOW, BAMMM, WORK

---
+++ SuddenTesties/Assets/code/monobehaviour/jumpThroughPlatform.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jumpThroughPlatform : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.layer = LayerMask.NameToLayer ("PlayerJumpthrough");
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D col){
+		if (col.gameObject.tag == "Player") {
+			col.gameObject.layer = LayerMask.NameToLayer ("Player");
+		}
+	}
+}
--- SuddenTesties/Assets/code/monobehaviour/playerMovement.cs
+++ SuddenTesties/Assets/code/monobehaviour/playerMovement.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class playerMovement : MonoBehaviour {
 	#region EditorVariables
-	[SerializeField] int maxSpeed;
+	[SerializeField] float maxSpeed;
 	[SerializeField] float acceleration;
+	[SerializeField] float jumpForce;
+	[SerializeField] Transform groundCheck;
 	#endregion
 
 	bool facingRight = true;
@@ -13,24 +15,42 @@ public class playerMovement : MonoBehaviour {
 	bool jump = false;
 	int xDir = 0; // Left: -1, Standstill: 0, Right: 1
 
+	Rigidbody2D playerRB;
+
 	// Use this for initialization
 	void Start () {
-		
+		playerRB = gameObject.GetComponent<Rigidbody2D> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		grounded = (Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("Ground")) ||
+		Physics2D.Linecast (transform.position, groundCheck.position, 1 << LayerMask.NameToLayer ("WorldBox")));
+
 		if (Input.GetKeyDown (KeyCode.D)) {
 			xDir = 1;
 		} else if (Input.GetKeyDown (KeyCode.A)) {
 			xDir = -1;
 		}
+		if ((Input.GetKeyUp (KeyCode.A) && xDir == -1) || (Input.GetKeyUp (KeyCode.D) && xDir == 1)) {
+			xDir = 0;
+		}
 		if (Input.GetKeyDown (KeyCode.W) && grounded) {
 			jump = true;
-		}
+		}	
 	}
 
 	void FixedUpdate() {
-		
+		if (xDir != 0 && (gameObject.GetComponent<Rigidbody2D> ().velocity.x * (float)xDir) < maxSpeed) {
+			gameObject.GetComponent<Rigidbody2D> ().AddForce (Vector2.right * xDir * acceleration);
+			if (playerRB.velocity.x > maxSpeed) {
+				playerRB.velocity = new Vector2 (Mathf.Sign (playerRB.velocity.x) * maxSpeed, playerRB.velocity.y);
+			}
+		}
+		if (jump) {
+			playerRB.AddForce (Vector2.up * jumpForce);
+			jump = false;
+		}
 	}
 }
