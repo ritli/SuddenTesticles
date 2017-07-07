decd2ba345396cb9c42b8fbcbdca6f173d0455b1
ritli <agnar_oskarsson@hotmail.com>
2017-07-08 01:20:40 +0200

    Initial commit

---
+++ SuddenTesties/Assets/code/monobehaviour/playerMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour {
+	#region EditorVariables
+	[SerializeField] int maxSpeed;
+	[SerializeField] float acceleration;
+	#endregion
+
+	bool facingRight = true;
+	bool grounded = false;
+	bool jump = false;
+	int xDir = 0; // Left: -1, Standstill: 0, Right: 1
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.D)) {
+			xDir = 1;
+		} else if (Input.GetKeyDown (KeyCode.A)) {
+			xDir = -1;
+		}
+		if (Input.GetKeyDown (KeyCode.W) && grounded) {
+			jump = true;
+		}
+	}
+
+	void FixedUpdate() {
+		
+	}
+}
