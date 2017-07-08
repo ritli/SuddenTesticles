cf60b70c8102ed93344fa0b3593289b98f9149b8
Nerfertius <nerfertius@gmail.com>
2017-07-08 23:52:28 +0200

    Jump anims and fixed ground trigger boxes

--- SuddenTesties/Assets/code/Non-monobehaviour/gameHandler.cs
+++ SuddenTesties/Assets/code/Non-monobehaviour/gameHandler.cs
@@ -30,13 +30,13 @@ static public class gameHandler {
 
 	static public Vector2 playerDeath(int playerId, bool points = true){
 		if (points) {
-			playerId == 1 ? m_p2score++ : m_p1score++;
+			//playerId == 1 ? m_p2score++ : m_p1score++;
 		}
 
 		return m_spawnPoints [Random.Range (0, m_spawnPoints.Count)];
 	}
 
 	static public void scoreText(GUIText textObj, int playerId){
-		playerId == 1 ? m_p1ScoreText = textObj : m_p2ScoreText = textObj;
+		//playerId == 1 ? m_p1ScoreText = textObj : m_p2ScoreText = textObj;
 	}
 }
