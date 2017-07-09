9e4157f6fea2aa409eb3a30077016edcefd0be01
Gustaf Andersson <c.gustaf@hotmail.se>
2017-07-09 12:49:54 +0200

    Game scene slowly coming together

--- SuddenTesties/Assets/code/Non-monobehaviour/gameHandler.cs
+++ SuddenTesties/Assets/code/Non-monobehaviour/gameHandler.cs
@@ -5,7 +5,8 @@ using UnityEngine.UI;
 
 static public class gameHandler {
 
-	static List<Vector2> m_spawnPoints;
+	static GameObject m_spawnPointsObj;
+	static List<Vector3> m_spawnPoints;
 	static int[] m_pScore = new int[2];
 	static public int P1Score {
 		get { return m_pScore[0]; }
@@ -18,8 +19,12 @@ static public class gameHandler {
 
 	// Setup new game
 	public static void instantiate(){
-		m_spawnPoints = new List<Vector2> ();
-		// Load spawnpoints
+		m_spawnPointsObj = GameObject.Instantiate (Resources.Load ("SpawnPoints")) as GameObject;
+		m_spawnPoints = new List<Vector3> ();
+
+		foreach (Transform child in m_spawnPointsObj.transform) {
+			m_spawnPoints.Add (child.position);
+		}
 
 		// Load map
 
@@ -29,16 +34,17 @@ static public class gameHandler {
 		
 	}
 
-	static public Vector2 playerDeath(int playerId, bool points = true){
+	static public Vector3 playerDeath(int playerId, bool points = true){
 		if (points) {
-			m_pScore [(playerId == 1) ? 0 : 1]++;
+			m_pScore [playerId - 1]++;
+			m_scoreText [playerId - 1].text = m_pScore [playerId - 1].ToString ();
 		}
 
 		return m_spawnPoints [Random.Range (0, m_spawnPoints.Count)];
 	}
 
 	static public void setScoreText(Text textObj, int playerId){
-		m_scoreText [playerId == 1 ? 0 : 1] = textObj;
+		m_scoreText [playerId - 1] = textObj;
 		textObj.text = 0.ToString();
 	}
 }
---
+++ SuddenTesties/Assets/code/monobehaviour/fetchCoords.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fetchCoords : MonoBehaviour {
+
+	void OnGUI()
+	{
+		Vector3 p = new Vector3();
+		Camera  c = Camera.main;
+		Event   e = Event.current;
+		Vector2 mousePos = new Vector2();
+
+		// Get the mouse position from Event.
+		// Note that the y position from Event is inverted.
+		mousePos.x = e.mousePosition.x;
+		mousePos.y = c.pixelHeight - e.mousePosition.y;
+
+		p = c.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, c.nearClipPlane));
+
+		GUILayout.BeginArea(new Rect(20, 20, 250, 120));
+		GUILayout.Label("Screen pixels: " + c.pixelWidth + ":" + c.pixelHeight);
+		GUILayout.Label("Mouse position: " + mousePos);
+		GUILayout.Label("World position: " + p.ToString("F3"));
+		GUILayout.EndArea();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			Debug.Log (Input.mousePosition.x.ToString () + ", " + Input.mousePosition.y.ToString ());
+		}
+	}
+}
